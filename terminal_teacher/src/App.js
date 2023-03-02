e1e02e4ae4753489c0f7583714ead120a3dcce79
Matthew Munson <matthewmunson@Kims-MacBook-Pro.local>
2023-03-01 17:01:03 -0800

    Implemented arrow key behavior

--- terminal_teacher/src/App.js
+++ terminal_teacher/src/App.js
@@ -5,24 +5,58 @@ import TextField from '@mui/material/TextField';
 
 function App() {
 
-  const [currentState, setState] = React.useState({terminalContents: ""});
+  const [currentState, setState] = React.useState(
+    {terminalContents: "", 
+    commandList: [], 
+    selectedCommand: 0
+  });
 
   const handleTerminalInput = (event) => {
 
     const key = event.key;
 
+    // Enter behavior: Submit command, update terminal object, update command list
     if(key === "Enter") {
       event.preventDefault();
       
       const command = event.target.value;
 
-      let terminalContents = currentState.terminalContents
+      let terminalContents = currentState.terminalContents;
       terminalContents += "\n";
       terminalContents += command;
 
-      setState({terminalContents: terminalContents})
+      const commandList = currentState.commandList;
+      commandList.push(command);
+
+      setState(
+        {terminalContents: terminalContents, 
+          commandList: commandList,
+          selectedCommand: commandList.length
+        })
 
       event.target.value = "";
+    }
+    // Arrow keys get previous/next commands in list
+    else if(key === "ArrowUp" || key === "ArrowDown") {
+      const commandList = currentState.commandList;
+
+      let newCommandIndex;
+
+      if(key === "ArrowUp") {
+        newCommandIndex = currentState.selectedCommand - 1
+      }
+      else {
+        newCommandIndex = currentState.selectedCommand + 1
+      }
+
+      if(newCommandIndex >= 0 && newCommandIndex < commandList.length) {
+        event.target.value = commandList[newCommandIndex];
+        
+        const newState = currentState;
+        newState.selectedCommand = newCommandIndex;
+
+        setState(newState);
+      }
     }
   }
 
